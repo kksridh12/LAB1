525e7f1e9d3d394727f92037ae0cf2119a7ce5cd
a34137_asu <a34137_asu@nc-asu6-l04.apporto.com>
2026-09-17 04:29:15 +0000

    update signal names for almost rptr and wptrs

--- rptr_empty.sv
+++ rptr_empty.sv
@@ -7,7 +7,7 @@ module rptr_empty #(parameter ADDRSIZE = 4)
     output logic                almost_rempty,
     output logic [ADDRSIZE-1:0] raddr,
     output logic [ADDRSIZE :0]  rptr,
-    output logic [ADDRSIZE :0]  rptr_ae,
+    output logic [ADDRSIZE :0]  rptr_af,
     input  logic [ADDRSIZE :0]  rq2_wptr,
     input  logic [ADDRSIZE :0]  rq2_wptr_ae,
     input  logic                rinc,
@@ -15,26 +15,26 @@ module rptr_empty #(parameter ADDRSIZE = 4)
     input  logic                rrst_n);
     
     logic [ADDRSIZE:0] rbin;
-    logic [ADDRSIZE:0] rgraynext, rbinnext, rbin_ae_next, rgray_ae_next;
+    logic [ADDRSIZE:0] rgraynext, rbinnext, rbin_af_next, rgray_af_next;
     logic rempty_val;
     logic almost_rempty_val;
     
-    // ALMOST EMPTY is when FIFO is 1/4 empty.
-    localparam logic [ADDRSIZE:0] ALMOST_EMPTY = (ADDRSIZE == 2) ? 1 : ((1 << ADDRSIZE) / 4);
+    // ALMOST is when FIFO is 3/4 FULL.
+    localparam logic [ADDRSIZE:0] ALMOST = (ADDRSIZE == 2) ? 1 : ((1 << ADDRSIZE) / 4);
     //-------------------
     // GRAYSTYLE2 pointer
     //-------------------
     always_ff @(posedge rclk or negedge rrst_n)
-       if (!rrst_n) {rbin, rptr, rptr_ae} <= 0;
-       else {rbin, rptr, rptr_ae} <= {rbinnext, rgraynext, rgray_ae_next};
+       if (!rrst_n) {rbin, rptr, rptr_af} <= 0;
+       else         {rbin, rptr, rptr_af} <= {rbinnext, rgraynext, rgray_af_next};
     
     // Memory read-address pointer (okay to use binary to address memory)
     always_comb begin
         raddr     = rbin[ADDRSIZE-1:0];
         rbinnext  = rbin + (rinc & ~rempty);
-        rbin_ae_next = rbinnext - ALMOST_EMPTY;
+        rbin_af_next = rbinnext - ALMOST;
         rgraynext = (rbinnext>>1) ^ rbinnext;
-        rgray_ae_next = (rbin_ae_next>>1) ^ rbin_ae_next;
+        rgray_af_next = (rbin_af_next>>1) ^ rbin_af_next;
     end
     
     //---------------------------------------------------------------
--- wptr_full.sv
+++ wptr_full.sv
@@ -8,7 +8,7 @@ module wptr_full #(parameter ADDRSIZE = 4)
     output logic [ADDRSIZE-1:0] waddr,
     output logic [ADDRSIZE :0]  wptr,
     output logic [ADDRSIZE :0]  wptr_ae,
-    input  logic [ADDRSIZE :0]  wq2_rptr_ae,
+    input  logic [ADDRSIZE :0]  wq2_rptr_af,
     input  logic [ADDRSIZE :0]  wq2_rptr,
     input  logic                winc, 
     input  logic                wclk, 
@@ -19,20 +19,20 @@ module wptr_full #(parameter ADDRSIZE = 4)
     logic              wfull_val;
     logic              almost_wfull_val;
 
-    // Threshold in Gray code: distance from the full condition.
-    localparam logic [ADDRSIZE:0] ALMOST_FULL = (ADDRSIZE == 2) ? 1 : ((1 << ADDRSIZE) / 4);
+    // ALMOST is when FIFO is 3/4 EMPTY.
+    localparam logic [ADDRSIZE:0] ALMOST = (ADDRSIZE == 2) ? 1 : ((1 << ADDRSIZE) / 4);
     
     // GRAYSTYLE2 pointer
     always_ff @(posedge wclk or negedge wrst_n)
        if (!wrst_n) {wbin, wptr, wptr_ae} <= 0;
-       else {wbin, wptr, wptr_ae} <= {wbinnext, wgraynext, wgray_ae_next};
+       else         {wbin, wptr, wptr_ae} <= {wbinnext, wgraynext, wgray_ae_next};
     
     // Memory write-address pointer (okay to use binary to address memory)
     always_comb begin
         waddr     = wbin[ADDRSIZE-1:0];
         wbinnext  = wbin + (winc & ~wfull);
         wgraynext = (wbinnext>>1) ^ wbinnext;
-        wbin_ae_next = wbinnext - ALMOST_FULL;
+        wbin_ae_next = wbinnext - ALMOST;
         wgray_ae_next = (wbin_ae_next>>1) ^ wbin_ae_next; 
     end
     
@@ -44,7 +44,7 @@ module wptr_full #(parameter ADDRSIZE = 4)
     //------------------------------------------------------------------
     always_comb begin
         wfull_val          = (wgraynext == {~wq2_rptr[ADDRSIZE:ADDRSIZE-1], wq2_rptr[ADDRSIZE-2:0]});
-        almost_wfull_val   = winc & (wgraynext == {~wq2_rptr_ae[ADDRSIZE:ADDRSIZE-1], wq2_rptr_ae[ADDRSIZE-2:0]});
+        almost_wfull_val   = winc & (wgraynext == {~wq2_rptr_af[ADDRSIZE:ADDRSIZE-1], wq2_rptr_af[ADDRSIZE-2:0]});
     end
     always_ff @(posedge wclk or negedge wrst_n)
        if (!wrst_n) {wfull, almost_wfull} <= {1'b0, 1'b0};
